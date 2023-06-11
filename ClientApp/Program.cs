9177539b7ce93b0a4c2f6d6f667d9d98a6a0ebe3
dbjdbj <dbj@dbj.org>
2023-06-11 11:22:19 +0200

    client app works with proper values from azure portal

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -1,30 +1,66 @@
-using System;
+// using System;
 using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
+// using System.Net.Http;
+// using System.Net.Http.Headers;
+// using System.Threading.Tasks;
+using Microsoft.Identity.Client;
 using Serilog;
 
 namespace ClientApp
 {
     class Program
     {
+        // kong config depend on Kong config :)
         // Base URL of the Kong API Gateway
         static string kongBaseUrl = "http://localhost:8000";
 
         static string microserviceOneUrl { get { return kongBaseUrl + "/microservice-one"; } }
         static string microserviceTwoUrl { get { return kongBaseUrl + "/microservice-two"; } }
 
-        static HttpClient authorized_http_client { get {
-        // Create an instance of authorized HttpClientHanlder
-        var httpClientHandler = new HttpClientHandler
+        //
+        public static string AccessToken
         {
-            Credentials = new NetworkCredential("<username>", "<password>"),
-            UseDefaultCredentials = false
-        };
-        // and authorized http client using it
-        /* var httpClient = */ return new HttpClient(httpClientHandler);
-    } }
+            get
+            {
+                string tenant_id = "your app tennant id";
+                string clientId = "you app client id";
+                string clientSecret = "you app client secret";
+                string authority = $"https://login.microsoftonline.com/{tenant_id}";
+
+                // coming from ere
+                // https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/ProtectAnAPI/appId/5bc9ca56-057f-47a5-bd8e-212527d30223/isMSAApp~/false
+                string[] scopes = {
+                     "your scope uri"
+                };
+
+                IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create(clientId)
+                    .WithClientSecret(clientSecret)
+                    .WithAuthority(authority)
+                    .Build();
+
+                var authResult = app.AcquireTokenForClient(scopes).ExecuteAsync();
+
+                return authResult.Result.AccessToken;
+            }
+        }
+
+
+
+        static HttpClient authorized_http_client
+        {
+            get
+            {
+                // Create an instance of authorized HttpClientHanlder
+                var httpClientHandler = new HttpClientHandler
+                {
+                    Credentials = new NetworkCredential("<local user>", "<pwd>"),
+                    UseDefaultCredentials = false
+                };
+                // and authorized http client using it
+                /* var httpClient = */
+                return new HttpClient(httpClientHandler);
+            }
+        }
 
         static async Task Main(string[] args)
         {
@@ -35,22 +71,24 @@ namespace ClientApp
 
             try
             {
-                    // Make a request to the desired microservice through the Kong API Gateway
-                    await CallMicroservice(microserviceOneUrl);
-                    await CallMicroservice(microserviceTwoUrl);
-          
-            } catch (Exception ex)
+                string accessToken = AccessToken;
+                // Make a request to the desired microservice through the Kong API Gateway
+                await CallMicroservice(microserviceOneUrl, accessToken);
+                await CallMicroservice(microserviceTwoUrl, accessToken);
+
+            }
+            catch (Exception ex)
             {
-                Log.Error($"Exception caught in main(): {ex.Message}" );
+                Log.Error($"Exception caught in main(): {ex.Message}");
             }
         } // Main
 
-        static async Task CallMicroservice(string microserviceUrl)
+        static async Task CallMicroservice(string microserviceUrl, string _jwt_token)
         {
             var request = new HttpRequestMessage(HttpMethod.Get, microserviceOneUrl);
 
             // Add any necessary headers to the request
-            //request.Headers.Add("Authorization", $"Bearer {accessToken}");
+            request.Headers.Add("Authorization", $"Bearer {_jwt_token}");
 
             // Send the request and retrieve the response
             var response = await authorized_http_client.SendAsync(request);
